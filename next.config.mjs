dfd72384b2ee99bb9120c3419b50402cfeebd76f
Andy Nguyen <anhnt2310@AnhNT-MacBook-Pro.local>
2024-07-22 22:21:56 +0700

    feat: add dashboard page

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,18 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
